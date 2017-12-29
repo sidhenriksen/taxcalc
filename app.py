3bdd72ef3d3ad394f27da13629999c14c27e2c87
sidh0 <sid.henriksen@gmail.com>
2017-12-29 12:06:33 +0000

    Working prototype

---
+++ app.py
@@ -0,0 +1,112 @@
+import numpy as np
+import dash,pickle,requests
+import dash_core_components as dcc
+import dash_html_components as html
+
+import tax
+
+taxFunctions = {('scotland',2017) : tax.scotland_2017,
+         ('scotland',2018) : tax.scotland_2018,
+         ('ruk',2017) : tax.ruk_2017,
+         ('ruk',2018) : tax.ruk_2018}
+    
+app = dash.Dash()
+
+app.suppress_callback_exceptions=True
+app.config.update(
+    {'routes_pathname_prefix':'',
+     'requests_pathname_prefix':''})
+server = app.server
+
+
+layout = html.Div([
+    html.Div([
+        html.P('Country'),
+        dcc.Dropdown(id='country',
+                     options = [{'label':'Scotland','value':'scotland'},
+                                    {'label':'rUK','value':'ruk'}],   
+                     value='scotland'),
+                     
+        html.P('Tax year'),
+        dcc.Dropdown(id='year',
+                         options=[{'label':'2017/18','value':2017},
+                                       {'label':'2018/19','value':2018}],
+                         value=2017),
+
+        html.P('Income (£)'),
+        dcc.Input(id='tax-input',type='number',value=20000)],
+                 style={'width':'40%','display':'inline-block','vertical-align':'top'}),
+    html.Div(id='taxfield',style={'width':'48%','display':'inline-block'})
+    ],style={'width':'98%'})
+
+app.layout = layout
+
+
+@app.callback(
+    dash.dependencies.Output('taxfield','children'),
+    [dash.dependencies.Input('tax-input','value'),
+     dash.dependencies.Input('country','value'),
+     dash.dependencies.Input('year','value')])
+def update_tax(income,country,year):
+
+    income = np.double(income)
+    f = taxFunctions[(country,year)]
+    currentIncomeTax = f(income)
+    currentNI = tax.national_insurance(income)
+    takehomePay = income - (currentIncomeTax+currentNI)
+    print(type(income))
+    print(type(currentNI))
+
+
+    dispString = html.Table([
+        html.Tr([
+            html.Td(
+            'Income : %.1f.'%income)
+            ]),
+        html.Tr([
+            html.Td(
+            'Income tax : %.1f.'%currentIncomeTax)
+            ]),
+        html.Tr([
+            html.Td(
+            'Percentage paid in income tax: %.2f%%.'%(currentIncomeTax/income*100))
+            ]),
+            
+        html.Tr([html.Td(html.Hr())]),
+
+        html.Tr([
+            html.Td(
+                'National insurance: %.1f.'%currentNI)
+            ]),
+            
+        html.Tr([
+            html.Td(
+            'Percentage paid in NI: %.2f%%.'%(currentNI/income*100))
+            ]),
+            
+        html.Tr([html.Td(html.Hr())]),
+
+        html.Tr([
+            html.Td(
+                'Take-home pay: %.1f.'%takehomePay)
+            ]),
+            
+        html.Tr([
+            html.Td(
+                'Total paid in tax: %.1f.'%(currentNI+currentIncomeTax)
+                )
+            ]),
+            
+        html.Tr([
+            html.Td(
+                'Percentage paid in tax: %.1f%%.'%((currentNI+currentIncomeTax)/income*100)
+                )
+            ])
+    ],style={'padding':'10px'})
+        
+         
+    return dispString
+    
+if __name__ == "__main__":
+
+    app.run_server(debug=True)
---
+++ tax.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def calculate_tax_rates(x,bands,maxAllowance=123000):
+    
+    taxBands = np.diff([0] + [k[0] for k in bands])
+    taxRates = np.array([k[1] for k in bands])
+    
+    # tapering personal allowance
+    taperedPA = np.max([0,x-maxAllowance])
+    adjustedPA = taxBands[0] - taperedPA/2
+    taxBands[0] = np.max([0,adjustedPA])
+    
+    payInBand = np.zeros(len(bands))
+    remaining = x
+    for i,band in enumerate(taxBands):
+        
+        inBand = remaining if band > remaining else band
+        remaining -= inBand
+        
+        payInBand[i] = inBand
+        
+        if remaining == 0:
+            break
+    
+    
+    if x > maxAllowance:
+        taxRates[0] = taxRates[1]
+        
+    totalTaxes = taxRates.dot(payInBand)
+    
+    return totalTaxes
+
+def scotland_2018(x):
+    band0 = (11850,0.0) # personal allowance
+    band1 = (13850,0.19)
+    band2 = (24000,0.2)
+    band3 = (44273,0.21)
+    band4 = (150000,0.41)
+    band5 = (np.inf,0.46)
+    
+    bands = [band0,band1,band2,band3,band4,band5]
+    return calculate_tax_rates(x,bands)
+
+def scotland_2017(x):
+    band0 = [11500,0.0]
+    band1 = [43000,0.2]
+    band2 = [150000,0.4]
+    band3 = [np.inf,0.45]
+
+    bands = [band0,band1,band2,band3]
+
+    return calculate_tax_rates(x,bands)
+
+def ruk_2018(x):
+    band0 = [11850,0.0] # personal allowance
+    band1 = [46350,0.2]
+    band2 = [150000,0.4]
+    band3 = [np.inf,0.45]
+    
+    bands = [band0,band1,band2,band3]
+    return calculate_tax_rates(x,bands)
+
+def ruk_2017(x):
+    band0 = [11500,0.0]
+    band1 = [45000,0.2]
+    band2 = [150000,0.4]
+    band3 = [np.inf,0.45]
+    
+    bands = [band0,band1,band2,band3]
+    return calculate_tax_rates(x,bands)
+
+def national_insurance(x):
+    band0 = [8164,0.0]
+    band1 = [45032,0.12]
+    band2 = [np.inf,0.02]
+
+    bands = [band0,band1,band2]
+    return calculate_tax_rates(x,bands)
